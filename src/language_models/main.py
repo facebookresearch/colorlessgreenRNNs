Fix bug in the calculation of validation loss
The last row of tokens in data-source is never used as input data in the
last batch, but only used as targets. The total_loss should thus not be
divided by len(data_source) but by len(data_source) - 1.

src/language_models/main.py
@@ -90,7 +90,7 @@ def evaluate(data_source):
         total_loss += len(data) * nn.CrossEntropyLoss()(output_flat, targets).data
         hidden = repackage_hidden(hidden)
 
-    return total_loss[0] /len(data_source)
+    return total_loss[0] /len(data_source - 1)
 
 
 def train():